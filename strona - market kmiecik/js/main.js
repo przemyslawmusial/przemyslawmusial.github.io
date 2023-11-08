0457a17140fb79a1c2615344deb1b98e81b9835c
przemyslawmusial <przemekm@student.agh.edu.pl>
2023-11-08 19:38:27 +0100

    pliki strony

---
+++ strona - market kmiecik/js/main.js
@@ -0,0 +1,8 @@
+const mobileNav = document.querySelector("ul");
+const burgerIcon = document.querySelector(".burger");
+
+burgerIcon.addEventListener("click", () => {
+	mobileNav.classList.toggle("active");
+	burgerIcon.classList.toggle("active");
+});
+
