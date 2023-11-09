e30768f5f1e56fcf8b2b53245f4e063e77be6a10
przemyslawmusial <przemekm@student.agh.edu.pl>
2023-11-09 20:19:45 +0100

    próba struktury strony

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const mobileNav = document.querySelector("ul");
 const burgerIcon = document.querySelector(".burger");
 const offerBtn = document.querySelector(".submenu-btn");
 const submenuContent = document.querySelector(".submenu-content");
+const screenWidth = window.innerWidth;
 
 burgerIcon.addEventListener("click", () => {
 	mobileNav.classList.toggle("active");
 	burgerIcon.classList.toggle("active");
 });
 
-offerBtn.addEventListener("click", () => {
-	submenuContent.classList.toggle("opened");
-});
+// if (screenWidth < 992) {
+	offerBtn.addEventListener("click", () => {
+		submenuContent.classList.toggle("opened");
+	});
+// }
