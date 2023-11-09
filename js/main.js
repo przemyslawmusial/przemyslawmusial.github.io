87eabe7dd239300738fb398c58ab242a9c45ba10
przemyslawmusial <przemekm@student.agh.edu.pl>
2023-11-09 17:20:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const mobileNav = document.querySelector("ul");
 const burgerIcon = document.querySelector(".burger");
+const offerBtn = document.querySelector(".submenu-btn");
+const submenuContent = document.querySelector(".submenu-content");
+
+offerBtn.addEventListener("click", () => {
+	submenuContent.classList.toggle("opened");
+});
 
 burgerIcon.addEventListener("click", () => {
 	mobileNav.classList.toggle("active");
 	burgerIcon.classList.toggle("active");
 });
-
