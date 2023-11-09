ea92fc619b4fde6ad9d171e110c120d558e822eb
przemyslawmusial <przemekm@student.agh.edu.pl>
2023-11-09 17:28:38 +0100

    poprawki ciąg dalszy

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const burgerIcon = document.querySelector(".burger");
 const offerBtn = document.querySelector(".submenu-btn");
 const submenuContent = document.querySelector(".submenu-content");
 
-offerBtn.addEventListener("click", () => {
-	submenuContent.classList.toggle("opened");
-});
-
 burgerIcon.addEventListener("click", () => {
 	mobileNav.classList.toggle("active");
 	burgerIcon.classList.toggle("active");
+});
+
+offerBtn.addEventListener("click", () => {
+	submenuContent.classList.toggle("opened");
 });
