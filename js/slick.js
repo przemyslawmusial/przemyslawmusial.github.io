a7d197221fb73e606d96736b452a64d4a0e9c911
przemyslawmusial <przemyslaw.musial.2@gmail.com>
2025-04-01 21:05:17 +0200

    update ranczo

--- js/slick.js
+++ js/slick.js
@@ -44,4 +44,121 @@ $(document).ready(function () {
 			// },
 		],
 	});
+	$(".gallery-container1 .house-carousele").slick({
+		mobileFirst: true,
+		arrows: true,
+		prevArrow: $(".gallery-container1 .carousele-arrows .fa-chevron-left"),
+		nextArrow: $(".gallery-container1 .carousele-arrows .fa-chevron-right"),
+		dots: false,
+		autoplay: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false,
+		centerMode: false,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2.2,
+					speed: 600,
+					arrows: true,
+					prevArrow: $(
+						".gallery-container1 .carousele-arrows .fa-chevron-left"
+					),
+					nextArrow: $(
+						".gallery-container1 .carousele-arrows .fa-chevron-right"
+					),
+				},
+			},
+		],
+	});
+
+	$(".gallery-container2 .house-carousele").slick({
+		mobileFirst: true,
+		arrows: true,
+		prevArrow: $(".gallery-container2 .carousele-arrows .fa-chevron-left"),
+		nextArrow: $(".gallery-container2 .carousele-arrows .fa-chevron-right"),
+		dots: false,
+		autoplay: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false,
+		centerMode: false,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2.2,
+					speed: 600,
+					arrows: true,
+					prevArrow: $(
+						".gallery-container2 .carousele-arrows .fa-chevron-left"
+					),
+					nextArrow: $(
+						".gallery-container2 .carousele-arrows .fa-chevron-right"
+					),
+				},
+			},
+		],
+	});
+	$(".gallery-container3 .house-carousele").slick({
+		mobileFirst: true,
+		arrows: true,
+		prevArrow: $(".gallery-container3 .carousele-arrows .fa-chevron-left"),
+		nextArrow: $(".gallery-container3 .carousele-arrows .fa-chevron-right"),
+		dots: false,
+		autoplay: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false,
+		centerMode: false,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2.2,
+					speed: 600,
+					arrows: true,
+					prevArrow: $(
+						".gallery-container3 .carousele-arrows .fa-chevron-left"
+					),
+					nextArrow: $(
+						".gallery-container3 .carousele-arrows .fa-chevron-right"
+					),
+				},
+			},
+		],
+	});
+	$(".gallery-container4 .house-carousele").slick({
+		mobileFirst: true,
+		arrows: true,
+		prevArrow: $(".gallery-container4 .carousele-arrows .fa-chevron-left"),
+		nextArrow: $(".gallery-container4 .carousele-arrows .fa-chevron-right"),
+		dots: false,
+		autoplay: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false,
+		centerMode: false,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2.2,
+					speed: 600,
+					arrows: true,
+					prevArrow: $(
+						".gallery-container4 .carousele-arrows .fa-chevron-left"
+					),
+					nextArrow: $(
+						".gallery-container4 .carousele-arrows .fa-chevron-right"
+					),
+				},
+			},
+		],
+	});
 });
